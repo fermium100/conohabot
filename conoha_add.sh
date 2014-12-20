434c1006800031a2e8a5a220c6d9eeb2f225dbec
Your Name <you@example.com>
2014-12-21 07:48:22 +0900

    ファイルが存在しなかった場合の例外処理

--- conoha_add.sh
+++ conoha_add.sh
@@ -3,4 +3,11 @@
 COFSCRVAR="
   robot.hear /XHEAR/, (msg) ->
     msg.send \"XSEND\""
+COFSCRVAR2="  robot.hear /XHEAR/, (msg) ->
+    msg.send \"XSEND\""
+if [ -e $1 ]; then
 echo "${COFSCRVAR}"|sed -e "s/XHEAR/$2/g; s/XSEND/$3/g" >> $1
+else
+echo "module.exports = (robot) ->" > $1
+echo "${COFSCRVAR2}"|sed -e "s/XHEAR/$2/g; s/XSEND/$3/g" >> $1
+fi
